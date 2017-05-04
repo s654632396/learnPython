b170a5137e7773d14248137fb4a0dfeecc483af5
DongChaofeng <s654632396@hotmail.com>
2017-05-04 23:23:39 +0800

    uheap

---
+++ heap/uheap.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+
+import math
+
+class UNode(object):
+    left = None
+    right = None
+
+
+    def __init__(self, item):
+        self.item = item
+
+class Uheap(object):
+    indcies = dict()
+
+    def __init__(self):
+        pass
+
+    def append(self,item):
+        pass
+
+    def pop(self):
+        pass
+
+
+
+
+
